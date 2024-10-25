5c4e1f53b811e4d2f0faad5f8be1f1261975a0f4
AmruthaMK <amrutha3217@gmail.com>
2024-10-25 09:14:19 +0530

    change message list AB

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello St.philomenaA!'));
+app.get('/', (req, res) => res.send('Hello St.philomenaAB!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
